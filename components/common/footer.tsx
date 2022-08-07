314dd581eb00f2fec7ed8737a1dfaec7fd1c9a2e
tin_pham <tin_pham@datahouse.com>
2022-08-07 23:44:45 +0700

    feat: Footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,10 +1,50 @@
 import * as React from 'react'
 import { Box } from '@mui/system'
+import { Icon, Stack, Typography } from '@mui/material'
+import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
+
+const socialLinks = [
+	{
+		icon: Facebook,
+		url: 'https://www.facebook.com/',
+	},
+	{
+		icon: Instagram,
+		url: 'https://www.instagram.com/',
+	},
+	{
+		icon: Twitter,
+		url: 'https://www.twitter.com/',
+	},
+	{
+		icon: LinkedIn,
+		url: 'https://www.linkedin.com/',
+	},
+]
 
 export function Footer() {
 	return (
 		<Box component={'footer'} py={2} textAlign="center">
-			Footer
+			<Stack direction="row" justifyContent={'center'}>
+				{socialLinks.map((link) => (
+					<Box
+						key={link.url}
+						component="a"
+						p={2}
+						href={link.url}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Icon
+							component={link.icon}
+							sx={{
+								fontSize: 48,
+							}}
+						/>
+					</Box>
+				))}
+			</Stack>
+			<Typography>Copyright ©{new Date().getFullYear()} All rights reserved </Typography>
 		</Box>
 	)
 }
